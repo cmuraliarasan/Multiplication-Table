fc050421eaab72a13f2767aeaed23dafbcbfea3f
Muraliarasan <66260672+cmuraliarasan@users.noreply.github.com>
2021-08-27 14:41:59 +0530

    Delete multiplication_table.py

--- multiplication_table.py
+++
@@ -1,9 +0,0 @@
-multiplicand_number=int(input("Enter the Multiplicand Number :"))
-multiplier_number = int(input("Enter the Multiplicand Number :"))
-
-def multiplication_table(multplicand_number,multiplier_number):
-   for i in range(1,multiplicand_number+1) :
-      print("%d * %d = %d" %(i,multiplier_number,i* multiplier_number))
-
-multiplication_table(multiplicand_number,multiplier_number)
-
