3251bd9b9d7a1855dce81172a62fd4f486e831ac
Muraliarasan <66260672+cmuraliarasan@users.noreply.github.com>
2021-08-27 14:42:55 +0530

    Add files via upload

---
+++ multiplication_table.py
@@ -0,0 +1,11 @@
+multiplicand_number=int(input("Enter the Multiplicand Number:"))
+multiplier_number = int(input(" :"))
+
+def multiplication_table(multplicand_number,multiplier_number):
+   for i in range(1,multiplicand_number+1) :
+      print("%d * %d = %d" %(i,multiplier_number,i* multiplier_number))
+
+multiplication_table(multiplicand_number,multiplier_number)
+
+
+
